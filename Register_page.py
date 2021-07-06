500c1c7ef53142125aa88a3bc0b34fef5605a6fe
Thabo Setsubi <thabosetsubi3@gmail.com>
2021-07-06 15:15:36 +0200

    Update

--- Register_page.py
+++ Register_page.py
@@ -3,7 +3,9 @@
 
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 import mysql.connector as mysql
+from datetime import datetime
 
 register = Tk()
 register.geometry("650x650")
@@ -20,13 +22,15 @@ class AddNewAccount:
         # Labels
         self.head_lab1 = Label(self.frame, text="LCA / LCS Login: ", font="arial 25", bg="#ffffff")
         self.head_lab1.place(x=20, y=10)
-        self.user_lab1 = Label(self.frame, text="Name: ")
-        self.user_lab1.place(x=20, y=100)
+        self.name_lab1 = Label(self.frame, text="Name: ")
+        self.name_lab1.place(x=20, y=100)
         self.pass_lab1 = Label(self.frame, text="Password: ")
         self.pass_lab1.place(x=20, y=130)
+        self.titles_lab = Label(self.frame, text="Department: ")
+        self.titles_lab.place(x=20, y=160)
         # Entries
-        self.user_entry = Entry(self.frame)
-        self.user_entry.place(x=80, y=100)
+        self.name_entry = Entry(self.frame)
+        self.name_entry.place(x=80, y=100)
         self.pass_entry = Entry(self.frame)
         self.pass_entry.place(x=80, y=130)
         # Buttons
@@ -34,6 +38,39 @@ class AddNewAccount:
         self.login_btn.place(x=20, y=180)
         self.return_btn = Button(self.frame, text="Return To Main Page", command=self.return_to_main)
         self.return_btn.place(x=80, y=180)
+        # Combobox
+        self.titles_cb = ttk.Combobox(self.frame)
+        self.titles = ["LCA", "LCS"]
+        self.titles_cb['values'] = self.titles
+        self.titles_cb['state'] = "readonly"
+        self.titles_cb.set("Select Department")
+        self.titles_cb.place(x=100, y=160)
+
+    # Function Adding new users
+    def login_func(self):
+        try:
+            db = mysql.connect(
+                host="localhost",
+                user="root",
+                passwd="Grimmijow06",
+                database="lifechoicesdb2"
+            )
+
+            today = datetime.today()
+
+            cursor = db.cursor()
+
+            query = "INSERT INTO students (name, password, date, title) VALUES (%s, %s, %s, %s)"
+
+            values = (self.name_entry.get(), self.pass_entry.get(), today, self.titles_cb.get())
+
+            cursor.execute(query, values)
+            db.commit()
+
+            messagebox.showinfo("Status", "You have successfully registered a new account at Life Choices")
+
+        except mysql.Error as err:
+            messagebox.showerror("Error", "Something went wrong: " + str(err))
 
     # Function Returning to the main page
     def return_to_main(self):
